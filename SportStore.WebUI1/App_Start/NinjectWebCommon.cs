07426e5c225253a99b3edb2b43e27b9d91d37c85
opilane <opilane@HP-CZC6297B9B>
2020-09-28 12:24:02 +0300

    Didnt working

---
+++ SportStore.WebUI1/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,17 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportStore.WebUI1.App_Start
+{
+    
+    public class NinjectWebCommon
+    {
+        private static void RegisterServices(IKernel kernel)
+        {
+            System.Web.Mvc.DependencyResolver.SetResolver(new SportsStore.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
+        }
+    }
+}
